cf79cb36f6e4160af08681f9e8217c98e7730485
root <root@localhost.localdomain>
2021-02-22 13:16:49 -0500

    Add existing file

---
+++ 4-start-libvirt-kvm.sh
@@ -0,0 +1,4 @@
+echo "This will start and enable libvirt"
+/usr/bin/systemctl start libvirtd
+/usr/bin/systemctl enable libvirtd
+/usr/bin/systemctl status libvirtd
---
+++ 5-mkdir-ocp4x.sh
@@ -0,0 +1,6 @@
+mkdir /root/ocp4 && cd /root/ocp4
+echo "Done"
+pwd
+echo "Fetching Openshift Container Platform instalation"
+sleep 1 
+git clone https://github.com/mikemcclure-ibm/Private-automagic-ocp4-installer-on-kvm.git
