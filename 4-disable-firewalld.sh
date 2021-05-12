64fb247b142e1f80791e6afb67a34498c5cd6508
Mike McClure <mikemcclure@us.ibm.com>
2021-05-12 08:39:48 -0400

    Delete 4-disable-firewalld.sh

--- 4-disable-firewalld.sh
+++
@@ -1,9 +0,0 @@
-clear
-echo  "------------------------------------------"
-echo "Now we are going to temporarily stop the Linux Firewall"
-echo "Hit ctrl+c to abort"
-sleep 2
-
-echo "systemctl stop firewalld"
-echo "systemctl disable firewalld"
-echo "check firewall with firewall-cmd --list-all"
