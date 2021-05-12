a8db9326f2c5364f6657a21252057dc8c02d17af
Mike McClure <mikemcclure@us.ibm.com>
2021-05-12 08:41:06 -0400

    Delete 8-mkdir-ocp4x.sh

--- 8-mkdir-ocp4x.sh
+++
@@ -1,8 +0,0 @@
-clear
-mkdir /root/ocp4 
-echo "Done"
-mkdir cd /root/ocp4/
-pwd
-echo "Fetching Openshift Container Platform instalation"
-sleep 1 
-git clone https://github.com/mikemcclure-ibm/Private-automagic-ocp4-installer-on-kvm.git
